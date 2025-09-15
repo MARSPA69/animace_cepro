a091afd135edfa95f462033bd9893d9668267c8f
Martin Spano <mspano1969@gmail.com>
2025-09-15 21:59:56 +0200

    funkční cross mode

---
+++ adminlte/dist/kopie složek/150920252157_funkční CROSS MODE/ANCHORID_TO_COMPASSANGLE.js
@@ -0,0 +1,420 @@
+// AUTO-GENERATED from ANCHORID_TO_COMPASSANGLE.xlsx
+// Generation parameters: ANGLE_TOLERANCE = 15
+
+window.ANCHOR_TO_COMPASS_TABLE = [
+  {
+    "code": 10,
+    "sequence": [
+      41,
+      42,
+      9,
+      43,
+      10,
+      47
+    ],
+    "sequenceKey": "41-42-9-43-10-47",
+    "compass_deg": 272.0,
+    "min_deg": 257.0,
+    "max_deg": 287.0,
+    "direction": "WEST"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      11,
+      12,
+      13
+    ],
+    "sequenceKey": "11-12-13",
+    "compass_deg": 293.0,
+    "min_deg": 278.0,
+    "max_deg": 308.0,
+    "direction": "WNW"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      14,
+      15,
+      16
+    ],
+    "sequenceKey": "14-15-16",
+    "compass_deg": 270.0,
+    "min_deg": 255.0,
+    "max_deg": 285.0,
+    "direction": "WEST"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      16,
+      17,
+      18
+    ],
+    "sequenceKey": "16-17-18",
+    "compass_deg": 354.0,
+    "min_deg": 339.0,
+    "max_deg": 9.0,
+    "direction": "NORTH"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      18,
+      19,
+      20,
+      21
+    ],
+    "sequenceKey": "18-19-20-21",
+    "compass_deg": 270.0,
+    "min_deg": 255.0,
+    "max_deg": 285.0,
+    "direction": "WEST"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      21,
+      22,
+      44,
+      23,
+      24,
+      48,
+      25,
+      26
+    ],
+    "sequenceKey": "21-22-44-23-24-48-25-26",
+    "compass_deg": 200.0,
+    "min_deg": 185.0,
+    "max_deg": 215.0,
+    "direction": "SSW"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      26,
+      27,
+      28,
+      29,
+      30,
+      31,
+      32,
+      49
+    ],
+    "sequenceKey": "26-27-28-29-30-31-32-49",
+    "compass_deg": 114.0,
+    "min_deg": 99.0,
+    "max_deg": 129.0,
+    "direction": "ESE"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      49,
+      32,
+      33,
+      34,
+      35,
+      36,
+      37,
+      45,
+      38
+    ],
+    "sequenceKey": "49-32-33-34-35-36-37-45-38",
+    "compass_deg": 64.0,
+    "min_deg": 49.0,
+    "max_deg": 79.0,
+    "direction": "ENE"
+  },
+  {
+    "code": 10,
+    "sequence": [
+      39,
+      40,
+      46
+    ],
+    "sequenceKey": "39-40-46",
+    "compass_deg": 200.0,
+    "min_deg": 185.0,
+    "max_deg": 215.0,
+    "direction": "SSW"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      47,
+      10,
+      43,
+      9,
+      42,
+      41
+    ],
+    "sequenceKey": "47-10-43-9-42-41",
+    "compass_deg": 92.0,
+    "min_deg": 77.0,
+    "max_deg": 107.0,
+    "direction": "EAST"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      13,
+      12,
+      11
+    ],
+    "sequenceKey": "13-12-11",
+    "compass_deg": 113.0,
+    "min_deg": 98.0,
+    "max_deg": 128.0,
+    "direction": "ENE"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      16,
+      15,
+      14
+    ],
+    "sequenceKey": "16-15-14",
+    "compass_deg": 90.0,
+    "min_deg": 75.0,
+    "max_deg": 105.0,
+    "direction": "EAST"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      18,
+      17,
+      16
+    ],
+    "sequenceKey": "18-17-16",
+    "compass_deg": 174.0,
+    "min_deg": 159.0,
+    "max_deg": 189.0,
+    "direction": "SOUTH"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      21,
+      20,
+      19,
+      18,
+      17
+    ],
+    "sequenceKey": "21-20-19-18-17",
+    "compass_deg": 90.0,
+    "min_deg": 75.0,
+    "max_deg": 105.0,
+    "direction": "EAST"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      26,
+      25,
+      48,
+      24,
+      23,
+      44,
+      22,
+      21
+    ],
+    "sequenceKey": "26-25-48-24-23-44-22-21",
+    "compass_deg": 20.0,
+    "min_deg": 5.0,
+    "max_deg": 35.0,
+    "direction": "NNE"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      49,
+      32,
+      31,
+      30,
+      29,
+      28,
+      27,
+      26
+    ],
+    "sequenceKey": "49-32-31-30-29-28-27-26",
+    "compass_deg": 293.0,
+    "min_deg": 278.0,
+    "max_deg": 308.0,
+    "direction": "WMW"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      38,
+      45,
+      37,
+      36,
+      35,
+      34,
+      33,
+      32,
+      49
+    ],
+    "sequenceKey": "38-45-37-36-35-34-33-32-49",
+    "compass_deg": 245.0,
+    "min_deg": 230.0,
+    "max_deg": 260.0,
+    "direction": "WSW"
+  },
+  {
+    "code": 11,
+    "sequence": [
+      46,
+      40,
+      39
+    ],
+    "sequenceKey": "46-40-39",
+    "compass_deg": 20.0,
+    "min_deg": 5.0,
+    "max_deg": 35.0,
+    "direction": "NNE"
+  }
+];
+
+window.ANCHOR_TO_COMPASS_BY_SEQUENCE = {
+  "41-42-9-43-10-47": {
+    "code": 10,
+    "compass_deg": 272.0,
+    "min_deg": 257.0,
+    "max_deg": 287.0,
+    "direction": "WEST"
+  },
+  "11-12-13": {
+    "code": 10,
+    "compass_deg": 293.0,
+    "min_deg": 278.0,
+    "max_deg": 308.0,
+    "direction": "WNW"
+  },
+  "14-15-16": {
+    "code": 10,
+    "compass_deg": 270.0,
+    "min_deg": 255.0,
+    "max_deg": 285.0,
+    "direction": "WEST"
+  },
+  "16-17-18": {
+    "code": 10,
+    "compass_deg": 354.0,
+    "min_deg": 339.0,
+    "max_deg": 9.0,
+    "direction": "NORTH"
+  },
+  "18-19-20-21": {
+    "code": 10,
+    "compass_deg": 270.0,
+    "min_deg": 255.0,
+    "max_deg": 285.0,
+    "direction": "WEST"
+  },
+  "21-22-44-23-24-48-25-26": {
+    "code": 10,
+    "compass_deg": 200.0,
+    "min_deg": 185.0,
+    "max_deg": 215.0,
+    "direction": "SSW"
+  },
+  "26-27-28-29-30-31-32-49": {
+    "code": 10,
+    "compass_deg": 114.0,
+    "min_deg": 99.0,
+    "max_deg": 129.0,
+    "direction": "ESE"
+  },
+  "49-32-33-34-35-36-37-45-38": {
+    "code": 10,
+    "compass_deg": 64.0,
+    "min_deg": 49.0,
+    "max_deg": 79.0,
+    "direction": "ENE"
+  },
+  "39-40-46": {
+    "code": 10,
+    "compass_deg": 200.0,
+    "min_deg": 185.0,
+    "max_deg": 215.0,
+    "direction": "SSW"
+  },
+  "47-10-43-9-42-41": {
+    "code": 11,
+    "compass_deg": 92.0,
+    "min_deg": 77.0,
+    "max_deg": 107.0,
+    "direction": "EAST"
+  },
+  "13-12-11": {
+    "code": 11,
+    "compass_deg": 113.0,
+    "min_deg": 98.0,
+    "max_deg": 128.0,
+    "direction": "ENE"
+  },
+  "16-15-14": {
+    "code": 11,
+    "compass_deg": 90.0,
+    "min_deg": 75.0,
+    "max_deg": 105.0,
+    "direction": "EAST"
+  },
+  "18-17-16": {
+    "code": 11,
+    "compass_deg": 174.0,
+    "min_deg": 159.0,
+    "max_deg": 189.0,
+    "direction": "SOUTH"
+  },
+  "21-20-19-18-17": {
+    "code": 11,
+    "compass_deg": 90.0,
+    "min_deg": 75.0,
+    "max_deg": 105.0,
+    "direction": "EAST"
+  },
+  "26-25-48-24-23-44-22-21": {
+    "code": 11,
+    "compass_deg": 20.0,
+    "min_deg": 5.0,
+    "max_deg": 35.0,
+    "direction": "NNE"
+  },
+  "49-32-31-30-29-28-27-26": {
+    "code": 11,
+    "compass_deg": 293.0,
+    "min_deg": 278.0,
+    "max_deg": 308.0,
+    "direction": "WMW"
+  },
+  "38-45-37-36-35-34-33-32-49": {
+    "code": 11,
+    "compass_deg": 245.0,
+    "min_deg": 230.0,
+    "max_deg": 260.0,
+    "direction": "WSW"
+  },
+  "46-40-39": {
+    "code": 11,
+    "compass_deg": 20.0,
+    "min_deg": 5.0,
+    "max_deg": 35.0,
+    "direction": "NNE"
+  }
+};
+
+/**
+ * Najde kompasový směr/úhel pro danou sekvenci kotev.
+ * @param {number[]} seq - např. [41,42,9,43,10,47]
+ * @returns {{code:any, compass_deg:number, min_deg:number, max_deg:number, direction:string}|null}
+ */
+window.findCompassForSequence = function(seq) {
+  if (!Array.isArray(seq) || seq.length === 0) return null;
+  const key = seq.join("-");
+  return ANCHOR_TO_COMPASS_BY_SEQUENCE[key] ?? null;
+}
